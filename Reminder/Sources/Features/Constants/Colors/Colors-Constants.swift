d2a6aa0d1c5064f73e597fc93b7a790b809a9205
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-05-12 19:13:48 -0300

    [feat] - Cria cores primarias e secundárias

--- Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
+++ Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
@@ -11,5 +11,5 @@ import UIKit
 public struct Colors {
     
     static let primaryRedBase = UIColor(red: 192/255, green: 38/255, blue: 54/255, alpha: 1)
-    st
+    static let secondaryRedLight = UIColor(red: 235/255, green: 60/255, blue: 77/255, alpha: 1)
 }
