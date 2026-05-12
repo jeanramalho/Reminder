5983482413383e4c4e77efb2e72e77935c63c8cf
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-05-12 19:17:47 -0300

    [feat] - Cria cor azul secundária

--- Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
+++ Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
@@ -14,4 +14,5 @@ public struct Colors {
     static let secondaryRedLight = UIColor(red: 235/255, green: 60/255, blue: 77/255, alpha: 1)
     
     static let primaryBlueBase = UIColor(red: 51/255, green: 79/255, blue: 220/255, alpha: 1)
+    static let secondaryBlueLight = UIColor(red: 110/255, green: 131/255, blue: 236/255, alpha: 1)
 }
