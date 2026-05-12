885f01b174792da64527fdfd222f926bba1f75aa
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-05-12 19:43:50 -0300

    [feat] - Cria constants de metricas

---
+++ Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Metrics-Constants.swift
+//  Reminder
+//
+//  Created by Jean Ramalho on 12/05/26.
+//
+import Foundation
+import UIKit
+
+public struct Metrics {
+    
+    public static var huge: CGFloat = 48
+    public static var medium: CGFloat = 24
+    public static var small: CGFloat = 12
+    public static var tiny: CGFloat = 8
+    public static var little: CGFloat = 6
+    
+}
--- Reminder/Sources/Features/Splash/SplashView.swift
+++ Reminder/Sources/Features/Splash/SplashView.swift
@@ -17,14 +17,6 @@ class SplashView: UIView {
         return image
     }()
     
-    private let titleLogoLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Remind"
-        label.font = UIFont.boldSystemFont(ofSize: 32)
-        label.textColor = .white
-        return label
-    }()
     
     init() {
         super.init(frame: .zero)
@@ -44,16 +36,13 @@ class SplashView: UIView {
     
     private func setupHierarchy(){
         addSubview(logoImageView)
-        addSubview(titleLogoLabel)
     }
     
     private func setupConstraints(){
         NSLayoutConstraint.activate([
-            titleLogoLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: 24),
-            titleLogoLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             
-            logoImageView.trailingAnchor.constraint(equalTo: titleLogoLabel.leadingAnchor, constant: -8),
-            logoImageView.centerYAnchor.constraint(equalTo: titleLogoLabel.centerYAnchor),
+            logoImageView.trailingAnchor.constraint(equalTo: self.centerXAnchor, constant: <#T##CGFloat#>),
+            logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: <#T##CGFloat#>),
             
                        
         ])
