173319e63a10d0ce07c07740f8bd47e64f4202fa
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-06-24 15:50:16 -0300

    [feat] - Cria LoginBottomSheetView com estrutura primária

---
+++ Reminder/Sources/Features/LoginBottmSheet/LoginBottomSheetView.swift
@@ -0,0 +1,36 @@
+//
+//  LoginBottomSheetView.swift
+//  Reminder
+//
+//  Created by Jean Ramalho on 24/06/26.
+//
+import Foundation
+import UIKit
+
+class LoginBottomSheetView: UIView {
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        
+        setupHierachy()
+        setupConstraints()
+    }
+    
+    private func setupHierachy(){
+        
+    }
+    
+    private func setupConstraints(){
+        
+    }
+}
