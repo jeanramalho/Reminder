4e5f0b3a13997cfd594a070505fc362cf1cb6fcd
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-06-24 15:43:12 -0300

    [feat] - Cria LoginBottonSheetViewController

---
+++ Reminder/Sources/Features/LoginBottmSheet/LoginBottomSheetViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginBottomSheetViewController.swift
+//  Reminder
+//
+//  Created by Jean Ramalho on 24/06/26.
+//
+import Foundation
+import UIKit
+
+class LoginBottomSheetViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        setupConstraints()
+    }
+    
+    private func setupConstraints(){
+        
+    }
+    
+}
+
