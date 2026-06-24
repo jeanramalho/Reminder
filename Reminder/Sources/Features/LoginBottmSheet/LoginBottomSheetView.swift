8afdbfe763eba94677d13b6fa0df8182fdcdde66
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-06-24 16:15:20 -0300

    [feat] - Cria bottonSheet e seus elementos

--- Reminder/Sources/Features/LoginBottmSheet/LoginBottomSheetView.swift
+++ Reminder/Sources/Features/LoginBottmSheet/LoginBottomSheetView.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class LoginBottomSheetView: UIView {
     
+    private let handleArea: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .lightGray
+        view.layer.cornerRadius = Metrics.tiny
+        
+        return view
+    }()
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Entre para acessar suas receitas"
+        return label
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "email@exemplo.com"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.isSecureTextEntry = true
+        textField.placeholder = "senha"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = Colors.primaryRedBase
+        button.layer.cornerRadius = Metrics.tiny
+        return button
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
-        
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
@@ -22,15 +61,42 @@ class LoginBottomSheetView: UIView {
     
     private func setupUI(){
         
+        self.backgroundColor = .white
+        self.layer.cornerRadius = Metrics.small
+        
         setupHierachy()
         setupConstraints()
     }
     
     private func setupHierachy(){
-        
+        addSubview(titleLabel)
+        addSubview(emailTextField)
+        addSubview(passwordTextField)
+        addSubview(loginButton)
     }
     
     private func setupConstraints(){
-        
+        NSLayoutConstraint.activate([
+            
+            handleArea.topAnchor.constraint(equalTo: self.topAnchor, constant: Metrics.small),
+            handleArea.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            handleArea.widthAnchor.constraint(equalToConstant: 40),
+            handleArea.heightAnchor.constraint(equalToConstant: 6),
+            
+            titleLabel.topAnchor.constraint(equalTo: handleArea.bottomAnchor, constant: Metrics.medium),
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metrics.medium),
+            
+            emailTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Metrics.medium),
+            emailTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metrics.medium),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: Metrics.medium),
+            passwordTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metrics.medium),
+            
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: Metrics.medium),
+            loginButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metrics.medium),
+            loginButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Metrics.medium),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
+            
+        ])
     }
 }
