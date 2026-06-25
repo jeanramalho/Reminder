1050475a0cce73a74f38efdcc0a76c7ae62f9e6c
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-06-25 16:10:54 -0300

    [feat] - Inicia criação de efeitos de exibição do modal de login

--- Reminder/Sources/Features/LoginBottmSheet/LoginBottomSheetViewController.swift
+++ Reminder/Sources/Features/LoginBottmSheet/LoginBottomSheetViewController.swift
@@ -37,7 +37,7 @@ class LoginBottomSheetViewController: UIViewController {
             
         ])
         
-        
+        let heightConstraint = contentView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.5).isActive = true
         
     }
     
@@ -45,5 +45,19 @@ class LoginBottomSheetViewController: UIViewController {
         
     }
     
+    private func handlePanGesture(){
+        
+    }
+    
+    private func animateShow(completion: (() -> Void)? = nil) {
+        self.view.layoutIfNeeded()
+        contentView.transform = CGAffineTransform(translationX: 0, y: contentView.frame.height)
+        UIView.animate(withDuration: 0.3, animations: {
+            self.contentView.transform = .identity
+            self.view.layoutIfNeeded()
+        }) { _ in
+            completion?()
+        }
+    }
 }
 
