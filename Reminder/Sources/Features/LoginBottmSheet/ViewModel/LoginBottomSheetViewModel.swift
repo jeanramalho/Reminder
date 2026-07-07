03989791c809e1bb4f611fa0074e8bf88039d1fb
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-07-07 17:23:48 -0300

    [feat] - Cria primeira versão simples da autenticação

--- Reminder/Sources/Features/LoginBottmSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Reminder/Sources/Features/LoginBottmSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -5,11 +5,17 @@
 //  Created by Jean Ramalho on 06/07/26.
 //
 import Foundation
-import Firebase
+import FirebaseAuth
 
 class LoginBottomSheetViewModel {
 
     func doAuth(userNameLogin: String, password: String) {
-        
+        Auth.auth().signIn(withEmail: <#T##String#>, password: <#T##String#>) { [weak self] authResult, error in
+            if let error = error {
+                print("autenticacao nao foi concluida com sucesso \(error)")
+            } else {
+                print(authResult, "Sucesso na autenticação")
+            }
+        }
     }
 }
