38ca1ea146f3395b35352d59fe6f9ac48e12ba36
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-07-13 14:12:39 -0300

    [feat] - Cria estrutura basica para flowController

--- Reminder/ReminderFlowController.swift
+++ Reminder/ReminderFlowController.swift
@@ -5,12 +5,26 @@
 //  Created by Jean Ramalho on 13/07/26.
 //
 import Foundation
+import UIKit
 
 class ReminderFlowController {
     
 //MARK: - Properties
+    private var navigationController: UINavigationController?
     
 //MARK: - Splash
     
 //MARK: - Login
+    
+//MARK: - init
+    public init() {
+        
+    }
+    
+//MARK: - startFlow
+    func start() -> UINavigationController? {
+        let startViewController = SplashViewController()
+        self.navigationController = UINavigationController(rootViewController: startViewController)
+        return navigationController
+    }
 }
