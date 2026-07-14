bdaea28d805fbeac792f3e46e9f65ad35308293a
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-07-14 12:09:31 -0300

    [feat] - Cria SplashFlowDelegate

---
+++ Reminder/Sources/Features/Splash/SplashFlowDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SplashFlowDelegate.swift
+//  Reminder
+//
+//  Created by Jean Ramalho on 14/07/26.
+//
+import Foundation
+
+
+public protocol SplashFlowDelegate: AnyObject {
+    func openLoginBottomSheet()
+}
