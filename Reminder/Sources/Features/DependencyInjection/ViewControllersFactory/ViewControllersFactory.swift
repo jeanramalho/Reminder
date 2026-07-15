38f69fc1958c311e1a1609c7bf28c2c23a8c0189
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-07-15 15:53:01 -0300

    [feat] - Cria ViewControllersFactory

---
+++ Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactory.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllersFactory.swift
+//  Reminder
+//
+//  Created by Jean Ramalho on 15/07/26.
+//
+import Foundation
+import UIKit
+
+final class ViewControllersFactory: ViewControllersFactoryProtocol {
+    
+    func makeSplashViewController() -> SplashViewController {
+        <#code#>
+    }
+    
+    func makeLoginBottomSheetViewController() -> LoginBottomSheetViewController {
+        <#code#>
+    }
+    
+    
+}
