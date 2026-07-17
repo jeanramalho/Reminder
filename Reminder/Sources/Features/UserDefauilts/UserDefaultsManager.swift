1cdff248537356a3bee2fc037680b1dcc36dd5e4
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-07-17 16:17:00 -0300

    [feat] - Cria UserDefaultsManager para gerenciar as chaves do UserDefaults

---
+++ Reminder/Sources/Features/UserDefauilts/UserDefaultsManager.swift
@@ -0,0 +1,11 @@
+//
+//  UserDefaultsManager.swift
+//  Reminder
+//
+//  Created by Jean Ramalho on 17/07/26.
+//
+import Foundation
+
+class UserDefaultsManager {
+    private static let userKey = "userKey"
+}
