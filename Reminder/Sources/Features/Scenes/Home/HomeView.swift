953e2c1a978a104091adeddb5f768fbe4afc1e7a
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-07-23 14:19:38 -0300

    [feat] - Inicia a criação da HomeView

--- Reminder/Sources/Features/Scenes/Home/HomeView.swift
+++ Reminder/Sources/Features/Scenes/Home/HomeView.swift
@@ -9,4 +9,27 @@ import UIKit
 
 class HomeView: UIView {
     
+    let profileBackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray6
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let contentBackground: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 20
+        view.layer.masksToBounds = true
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 35
+        return imageView
+    }()
 }
