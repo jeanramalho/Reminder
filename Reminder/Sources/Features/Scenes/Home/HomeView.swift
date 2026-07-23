ec7774f5522de0900fa756a33e8e7b4ed4ade520
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-07-23 14:56:46 -0300

    [feat] - Cria estrutura inicial da HomeView

--- Reminder/Sources/Features/Scenes/Home/HomeView.swift
+++ Reminder/Sources/Features/Scenes/Home/HomeView.swift
@@ -32,4 +32,60 @@ class HomeView: UIView {
         imageView.layer.cornerRadius = 35
         return imageView
     }()
+    
+    let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Boas Vindas"
+        label.textColor = .black
+        label.font = Typograph.input
+        return label
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = Typograph.subHeading
+        label.textColor = .black
+        return label
+    }()
+    
+    let feeebackButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Avaliar", for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 12
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView(){
+        setupHierarchy()
+        setupConstraints()
+    }
+    
+    private func setupHierarchy() {
+        
+        addSubview(profileBackground)
+        profileBackground.addSubview(profileImage)
+        profileBackground.addSubview(welcomeLabel)
+        profileBackground.addSubview(nameLabel)
+        
+        addSubview(contentBackground)
+        contentBackground.addSubview(feeebackButton)
+    }
+    
+    private func setupConstraints() {
+        
+    }
 }
