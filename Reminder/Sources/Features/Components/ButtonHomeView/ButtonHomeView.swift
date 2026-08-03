75d9cc6f758b4f31965627bfe1b1db36d7f2a28d
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-03 10:06:41 -0300

    [feat] - Cria todos os elementos de UI que o componente precisará

--- Reminder/Sources/Features/Components/ButtonHomeView/ButtonHomeView.swift
+++ Reminder/Sources/Features/Components/ButtonHomeView/ButtonHomeView.swift
@@ -9,5 +9,46 @@ import UIKit
 
 class ButtonHomeView: UIView {
     
+    private let iconView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = Colors.gray600
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = Typograph.subHeading
+        label.textColor = Colors.gray100
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let descriptionLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = Colors.gray200
+        label.font = Typograph.body
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let arroeImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.right")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.tintColor = Colors.gray300
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 }
 
