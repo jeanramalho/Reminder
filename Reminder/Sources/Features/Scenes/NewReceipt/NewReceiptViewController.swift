769b50d809ce9c506efb889f80200d060f5c9e25
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-05 09:08:14 -0300

    [feat] - Cria view controller NewReceiptViewController

---
+++ Reminder/Sources/Features/Scenes/NewReceipt/NewReceiptViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewReceiptViewController.swift
+//  Reminder
+//
+//  Created by Jean Ramalho on 05/08/26.
+//
+import Foundation
+import UIKit
+
+
+class NewReceiptViewController: UIViewController {
+    
+}
