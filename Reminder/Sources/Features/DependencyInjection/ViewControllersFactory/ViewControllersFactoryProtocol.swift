af622f4aa4c9e2145a56af6fabf048fc6d0186ce
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-10 09:17:40 -0300

    [feat] - Cria método de factory da NewRecipesViewController

--- Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactoryProtocol.swift
+++ Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactoryProtocol.swift
@@ -10,4 +10,5 @@ protocol ViewControllersFactoryProtocol: AnyObject {
     func makeSplashViewController(flowDelegate: SplashFlowDelegate) -> SplashViewController
     func makeLoginBottomSheetViewController(flowDelegate: LoginBottomSheetFlowDelegate) -> LoginBottomSheetViewController
     func makeHomeViewController(flowDelegate: HomeFlowDelegate) -> HomeViewController
+    func makeRecipesViewController() -> NewReceiptViewController
 }
