f96c70cfbd49c4e2ee1dddb2acc48d1cf0656be9
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-11 09:29:30 -0300

    [feat] - Cria DBHelper para banco de dados

---
+++ Reminder/Sources/Features/Database/DBHelper.swift
@@ -0,0 +1,15 @@
+//
+//  DBHelper.swift
+//  Reminder
+//
+//  Created by Jean Ramalho on 11/08/26.
+//
+import Foundation
+import SQLite3
+
+class DBHelper {
+    
+    static let shared = DBHelper()
+    
+    
+}
