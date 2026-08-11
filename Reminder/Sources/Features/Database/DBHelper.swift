64674a19576b1fa96ac58c7b3848733801ebaf19
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-11 09:35:48 -0300

    [feat] - Cria estrutua inicial para abrir banco de dados no helper

--- Reminder/Sources/Features/Database/DBHelper.swift
+++ Reminder/Sources/Features/Database/DBHelper.swift
@@ -10,6 +10,22 @@ import SQLite3
 class DBHelper {
     
     static let shared = DBHelper()
+    private var db: OpaquePointer?
+    
+    private init () {
+        openDatabase()
+        createTable()
+    }
+    
+    private func openDatabase() {
+        let fileURL = try! FileManager.default
+            .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("Reminder.sqlite")
+        
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("Erro ao abrir banco de dados")
+        }
+    }
     
     
 }
