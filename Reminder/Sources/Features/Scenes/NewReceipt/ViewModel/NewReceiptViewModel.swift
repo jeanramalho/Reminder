2512e7b51aef8386e03f508d81beb7f0fd77220a
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-13 09:27:27 -0300

    [feat] - Cria estrutura basica da funcao de salvar nova receita no banco de dados na ViewModel

--- Reminder/Sources/Features/Scenes/NewReceipt/ViewModel/NewReceiptViewModel.swift
+++ Reminder/Sources/Features/Scenes/NewReceipt/ViewModel/NewReceiptViewModel.swift
@@ -8,6 +8,6 @@ import Foundation
 
 class NewReceiptViewModel {
     func addReceipt(remedy: String, time: String, recurrence: String, takeNow: Bool) {
-        
+        DBHelper.shared.insertReceipt(remedy: remedy, time: time, recurrence: recurrence, takeNow: takeNow)
     }
 }
