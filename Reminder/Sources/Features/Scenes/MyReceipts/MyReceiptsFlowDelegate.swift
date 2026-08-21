5e26ade63a538aa445f8fd2e4c3784a4974d13f0
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-20 23:30:01 -0300

    [fix] - Ajusta nome de flowController para flowDelegate

--- Reminder/Sources/Features/Scenes/MyReceipts/MyReceiptsFlowDelegate.swift
+++ Reminder/Sources/Features/Scenes/MyReceipts/MyReceiptsFlowDelegate.swift
@@ -1,11 +1,11 @@
 //
-//  MyReceiptsFlowController.swift
+//  MyReceiptsFlowDelegate.swift
 //  Reminder
 //
 //  Created by Jean Ramalho on 20/08/26.
 //
 import Foundation
 
-public protocol MyReceiptsFlowController: AnyObject {
+public protocol MyReceiptsFlowDelegate: AnyObject {
     func goToNewReceipts()
 }
