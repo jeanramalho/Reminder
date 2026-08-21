e00fedbc28ec721291d6937338365d1e49382eb9
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-20 23:28:34 -0300

    [feat] - Cria MyReceiptsFlowController

---
+++ Reminder/Sources/Features/Scenes/MyReceipts/MyReceiptsFlowController.swift
@@ -0,0 +1,11 @@
+//
+//  MyReceiptsFlowController.swift
+//  Reminder
+//
+//  Created by Jean Ramalho on 20/08/26.
+//
+import Foundation
+
+public protocol MyReceiptsFlowController: AnyObject {
+    func goToNewReceipts()
+}
