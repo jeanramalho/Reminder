83f4ca63ea9e8b4e518477550e0f64889db6c3d4
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-20 23:32:54 -0300

    [feat] - Ajusta parametros da makeMyReceiptsViewController

--- Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactoryProtocol.swift
+++ Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactoryProtocol.swift
@@ -11,5 +11,5 @@ protocol ViewControllersFactoryProtocol: AnyObject {
     func makeLoginBottomSheetViewController(flowDelegate: LoginBottomSheetFlowDelegate) -> LoginBottomSheetViewController
     func makeHomeViewController(flowDelegate: HomeFlowDelegate) -> HomeViewController
     func makeRecipesViewController() -> NewReceiptViewController
-    func makeMyReceiptsViewController() -> MyReceiptsViewController
+    func makeMyReceiptsViewController(flowDelegate: MyReceiptsFlowDelegate) -> MyReceiptsViewController
 }
