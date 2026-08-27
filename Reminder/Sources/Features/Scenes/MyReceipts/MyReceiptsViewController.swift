6c3cf9cf4ec8e21a8922f2b606d91c994aaa8b9f
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-08-27 09:19:55 -0300

    [feat] - Cria mock para tableview

--- Reminder/Sources/Features/Scenes/MyReceipts/MyReceiptsViewController.swift
+++ Reminder/Sources/Features/Scenes/MyReceipts/MyReceiptsViewController.swift
@@ -11,6 +11,16 @@ class MyReceiptsViewController: UIViewController {
     let contentView: MyReceiptsView
     weak var flowDelegate: MyReceiptsFlowDelegate?
     
+    private let mockMedicamentos = [
+        ("buscopam, 13:00", "2 em 2 horas"),
+        ("lorazepam, 15:00", "4 em 4 horas"),
+        ("venlift, 17:00", "6 em 6 horas"),
+        ("depakote, 20:00", "1 vez ao dia"),
+        ("closapina, 22:00", "8 em 8 horas"),
+        ("litum, 4:00", "2 em 2 horas"),
+        
+    ]
+    
     init(contentView: MyReceiptsView, flowDelegate: MyReceiptsFlowDelegate) {
         self.contentView = contentView
         self.flowDelegate = flowDelegate
