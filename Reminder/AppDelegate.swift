7c33e0efd060d03f01d4d3672573256e55348d97
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-09-09 09:22:57 -0300

    [feat] - Cria método para pedir permissão de notificações

--- Reminder/AppDelegate.swift
+++ Reminder/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -31,6 +32,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func registerForNotifications() {
+        let centerNotification = UNUserNotificationCenter.current()
+        centerNotification.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if granted {
+                print("Permissão concedida para notificações.")
+            } else if let error = error {
+                print("\(error) Erro ao pegar permissões")
+            }
+            
+        }
     }
 
 
