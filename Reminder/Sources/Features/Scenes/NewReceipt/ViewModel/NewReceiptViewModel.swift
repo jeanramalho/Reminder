59e1c9c006dbc3bef92affbdce10b88535823e6a
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-09-10 09:15:54 -0300

    [feat] - Inicia desenvolvimento de funções para notificações

--- Reminder/Sources/Features/Scenes/NewReceipt/ViewModel/NewReceiptViewModel.swift
+++ Reminder/Sources/Features/Scenes/NewReceipt/ViewModel/NewReceiptViewModel.swift
@@ -10,4 +10,8 @@ class NewReceiptViewModel {
     func addReceipt(remedy: String, time: String, recurrence: String, takeNow: Bool) {
         DBHelper.shared.insertReceipt(remedy: remedy, time: time, recurrence: recurrence, takeNow: takeNow)
     }
+    
+    private func scheduleNotification() {
+        
+    }
 }
