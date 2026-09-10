7ab7285b3425514a890c3f7f4eb9a1718f8e8a0d
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-09-10 09:21:43 -0300

    [feat] - Cria switch para pegar intervalo entre as horas

--- Reminder/Sources/Features/Scenes/NewReceipt/ViewModel/NewReceiptViewModel.swift
+++ Reminder/Sources/Features/Scenes/NewReceipt/ViewModel/NewReceiptViewModel.swift
@@ -16,6 +16,29 @@ class NewReceiptViewModel {
     }
     
     private func getIntervalInHours(from recurrence: String) -> Int {
-        
+        switch recurrence {
+        case "De hora em hora":
+            return 1
+        case "2 em 2 horas":
+            return 2
+        case  "4 em 4 horas":
+            return 4
+        case "6 em 6 horas":
+            return 6
+        case "8 em 8 horas":
+            return 8
+        case "12 em 12 horas":
+            return 12
+        case "1 ao dia":
+            return 24
+            break
+        }
     }
+    
+    
+   
+    
+    
+    
+    
 }
